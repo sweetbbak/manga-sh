194c287d9614d3cd980fba85adca04bca0ccaa4d
alaska <75046310+sweetbbak@users.noreply.github.com>
2023-02-04 23:00:27 -0900

    Update asura.sh
    add better flow control, move files to cache, better/cleaner code and add python async image download script :)

--- asura.sh
+++ asura.sh
@@ -1,112 +1,171 @@
 #!/bin/bash
 
-version_number="0.1.0"
-site="https://www.asurascans.com/"
+url="https://www.asurascans.com/"
+version="0.1.0"
+cache_dir="$HOME"/.cache/asuradl/
+raw_html="$cache_dir"asura-homepg
+db_file="$cache_dir"asura-db.json
+agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
 
-die() {
-	printf "\33[2K\r\033[1;31m%s\033[0m\n" "$*" >&2
-	exit 1
+# -d is "if file exists and is a dir" - -f for file
+if [[ ! -d "$cache_dir" ]]; then
+  mkdir -p "$cache_dir"
+fi
+
+version_info() {
+  printf "%s" "$version"
 }
 
-trim_string() {
-    # Usage: trim_string "   example   string    "
-    : "${1#"${1%%[![:space:]]*}"}"
-    : "${_%"${_##*[![:space:]]}"}"
-    printf '%s\n' "$_"
+get_latest() {
+  # [ -f "$cache_dir"index.html ] && rm "$cache_dir"index.html
+  curl -s -A "$agent" "$url" > "$raw_html"
+  # cat "$cache_dir"index.html > "$raw_html"
+  home_pg=$(cat "$raw_html")
+  # rm "$cache_dir"index.html
 }
 
-# checks if dependencies are present
-dep_ch() {
-	for dep; do
-		command -v "$dep" >/dev/null || die "Program \"$dep\" not found. Please install it."
-	done
-}
-
-# update cached front page after 30 minutes
-is_older(){
-  lastup=$(stat -c %Y "$HOME"/src/asura-bash/asura-homepg)
+update_frontpage() {
+if [[ -f "$raw_html" ]]; then
+  get_latest
+else
+  lastup=$(stat -c %Y "$raw_html")
   now=$(date +%s)
   age=$((now - lastup))
+  # if file is over 30 mins old then get an update
   if (( age > 1850 )); then
+    printf "%s\n" "updating database..."
     get_latest
   else
-    home_pg=$(cat asura-homepg)
+    get_latest
+    # home_pg=$(cat "$raw_html")
   fi
+fi
 }
 
 get_ch_list() {
   ch_list=$(curl -s -A "$agent" "$1")
-}
-
-get_latest() {
-  [ -f index.html ] && rm index.html
-  # wget -O - ...prints to stdout
-  home_pg="$(wget -q -O index.html -U "$agent" -H "$site")"
-  cat index.html > asura-homepg
-  home_pg=$(cat asura-homepg)
-}
-
-grab_titles() {
-  #titles
-  # printf "%s" "$home_pg" | pup 'div[class="luf"] h4 text{}'
-  # title + href
-    printf "%s" "$home_pg" | pup 'div[class="luf"] [class="series"] json{}' | jq '.[].href'
-    printf "%s" "$home_pg" | pup 'div[class="luf"] [class="series"] json{}' | jq '.[].title'
-  # printf "%s" "$home_pg" | pup 'div[class="uta"] [class="series"] h4 json{} | jq '.[].text''
-}
-
-get_fp() {
-  # create key pair list of title and link to manwha page (with all chapters, desc, list of ch. etc...)
-  titles_links="$(printf "%s" "$home_pg" | pup 'div[class="luf"] [class="series"] json{}' | jq '.[] | "\(.title)" + "*" + "\(.href)"')"
-  printf "%s" "$titles_links" > "$db_file" 
-  choice_fp="$(printf "%s\n" "$titles_links" | fzf --cycle --with-nth 1 --delimiter '*' | cut -d'*' -f2 | sed 's/"//g')"
-  get_ch_list "$choice_fp"
-}
-
-iterate_fp() {
-  # the first jq[0-49] and the last empty .[0-2] iterate over links (use sed '/null/d' or grep -v 'null' rm null spc)
-  # printf "%s" "$home_pg" | pup 'div[class="luf"] json{} | jq ".[0] | .children[0] | .children | .[0] | .children[0] | .href"'
-  printf "%s" "$home_pg" | pup 'div[class="luf"] json{}' | jq '.[] | .children[1] | .children | .[0] | .children[].href' | sed /null/d
-}
-
-fzf_send() {
-  choice=$(printf "%s\n" "$1" | fzf --cycle --with-nth 4 --delimiter '/')
-}
-
-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
-dl_dir="/home/sweet/src/asura-bash/"
-download_dir="${dl_dir:-.}"
-db_file="/home/sweet/src/asura-bash/asura-db"
-
-opt_view_fp() {
-  is_older
-  xyz=$(iterate_fp)
-  fzf_send "$xyz"
-  printf "%s\n" "\n"
-  gum style --border double "$choice"
-  # download 
-}
-
-opt_choose() {
-  is_older
-  get_fp
-  rd_chapter="$(printf "%s\n" "$ch_list" | pup 'div[id="chapterlist"] a[href] json{}' | jq '.[].href' | fzf --cycle --with-nth 4 --delimiter '/' | sed 's/"//g' | tr -d '[:space:]')"
+  rd_chapter="$(printf "%s\n" "$ch_list" | pup 'div[id="chapterlist"] a[href] json{}' | jq '.[].href' | fzf --cycle --with-nth 4 --delimiter '/' | sed 's/"//g' | tr -d '[:space:]')" 
   gum style --border double --foreground 212 "$rd_chapter"
-download_chapter "$rd_chapter"
+  dl_images "$rd_chapter"
 }
 
-download_chapter() {
+download_images() {
   titledir="$(printf "%s" "$1" | cut -d '/' -f4 | sed 's/"//g' | tr -d '[:space:]')"
-  mkdir -p "$download_dir"/"$titledir" 
-  cd "$download_dir"/"$titledir" && wget -U "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0" -nd -r --level=1  -e robots=off -A jpg,jpeg -H "${rd_chapter}"
-  ouch compress "$(\ls -1v *.jpg)" temp.zip && mv temp.zip "$titledir".cbz
+  mkdir -p "$cache_dir""$titledir"
+  gum spin -- wget -P "$cache_dir""$titledir" -q -U "$agent" -nd -r --level=1 -e robots=off -A jpg,jepg -H "$1"
+  cd "$cache_dir""$titledir" && ouch compress "$(\ls -v *.jpg)" 
 }
 
-printf "%s\n" ""
-gum_opts="$(gum choose "view front page" "choose new" "bookmarked" "exit")"
-case "$gum_opts" in
-  "view front page") opt_view_fp ;;
-  "choose new") opt_choose ;;
-  "bookmarked") ;;
-  "exit") exit 0;;
+dl_images() {
+  gum style --foreground 212 'dl_images... mktemp & get images & async py dl'
+  tmp_file="$(mktemp -t mytemp-XXXXXX)"
+  echo "$tmp_file"
+  curl -s -A "$agent" "$1" > "$tmp_file"
+  cat "$tmp_file" | pup 'div[class="rdminimal"] p img json{}' | jq '.[].src'| sed 's/"//g' > "$HOME"/src/asura-bash/images.txt
+  rm "$tmp_file"
+  gum spin -- python3 "$HOME"/src/asura-bash/async-imgdl.py "$HOME"/src/asura-bash/images.txt
+}
+
+fp2_json() {
+  gum style --foreground 212 'fp2-json parsing front page'
+  results_raw_json=$(printf "%s\n" "$home_pg" | pup 'div[class="luf"] json{}')
+  # printf "%s" "$results_json" | jq '.[] | [.children[0].href, .children[0].title, "\(.children[1].children[].children[0].href)"]'
+  #print JSON of title, link and latest chapter :)
+  fp_json=$(printf "%s\n" "$results_raw_json" | jq '.[] | {title: .children[0].title, link: .children[0].href, latest: "\(.children[1].children[0].children[0].href)"}' | jq -s '.')
+  printf "%s\n" "$fp_json" > "$db_file"
+}
+
+read_data() {
+  # tmpfile=$(cat "$db_file")
+  cat "$db_file" | jq '.[].title' |\
+    fzf \
+      --cycle \
+      --bind 'o:execute(cat '"$db_file"' | jq ".[{n}].latest")+abort' \
+      --bind 'enter:execute(cat '"$db_file"' | jq ".[{n}].link")+abort' \
+      --preview='cat '"$db_file"' | jq ".[{n}]"'
+}
+
+read_handler() {
+  case "$1" in
+  *manga*)
+    # link to main page - send to show all chapters
+    get_ch_list "$@"
+    ;;
+  *chapter*)
+    # send to download chapter
+    dl_images "$@"
+    ;;
+  esac
+}
+
+bookmark() {
+  ls
+}
+
+update_exit() {
+  update_frontpage
+  fp2_json
+  dblines="$(cat "$db_file" | jq ".[].title" | wc -l)"
+  gum style --border double --foreground 212 "Updated ${dblines} entries to Asura Scans database"
+  # exit 0
+  sleep 1
+  fzf_main
+}
+
+zip_target() {
+  for target in "$@"; do
+    if [[ -e "$target" ]]; then # does exist
+      if [[ -r "$target" ]]; then # is readable
+        if [[ -d "$target" ]]; then  # is dir
+          archive=${target%/}
+          echo "Archiving: $target"
+          echo "zip -mTy9 \"$archive.cbz" \"$target"" # -x \"*.DS_Store\" \"*[Tt]humbs.db\"" 
+          zip -mTy9 "$archive.cbz" "$target" # -x "*.DS_Store" "*[Tt]humbs.db"
+        else
+          echo "Not a directory: $target"
+        fi
+      else
+        echo "Not readable: $target "
+      fi
+    else
+      echo "Not found: $target"
+    fi
+done
+}
+
+fzf_main() {
+  fzfopt="$(printf "%s\n" "1:Update chapters" "2:Pick manhwa" "3:Exit" | fzf --with-nth 2 --delimiter ":" | cut -d ':' -f1)"
+  case "$fzfopt" in
+    1)
+    update_exit
+    ;;
+    2)
+    main
+    ;;
+    3)
+    exit 0
+    ;;
+  esac
+}
+
+main() {
+  link_opt=$(read_data)
+  link_opt=$(echo "$link_opt"|sed 's/\"//g'|tr -d '[:space:]')
+  read_handler "$link_opt"
+}
+
+case "$1" in
+  -u|--update) 
+    update_exit
+  ;;
+  -v|--version)
+    version_info
+  ;;
+  -b|--bookmark)
+  bookmark
+  ;;
+  *)
+  fzf_main
+  ;;
+  
 esac
